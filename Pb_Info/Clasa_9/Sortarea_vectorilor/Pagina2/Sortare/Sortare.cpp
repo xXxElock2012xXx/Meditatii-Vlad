e39d8306616fcaca22b73ec9a74d979b286dd96b
xXxAloc2024kxXx <asancretu@gmail.com>
2024-01-24 14:40:24 +0200

    Added solutio for problema sortare

---
+++ Pb_Info/Clasa_9/Sortarea_vectorilor/Pagina2/Sortare/Sortare.cpp
@@ -0,0 +1,34 @@
+#include<iostream>
+#include<fstream>
+
+using namespace std;
+
+ifstream intr ("fis.in");
+
+int main()
+{
+    int x[1000],a;
+    intr>>a;
+    for(int i=0;i<a;i++){
+        intr>>x[i];
+    }
+
+    for(int i=0;i<a;i++){
+        int p=i;
+        for(int j=i;j<a;j++){
+            if(x[j]<x[i]){
+                p=j;
+            }
+        }
+        int max=x[i];
+        x[i]=x[p];
+        x[p]=max;
+    }
+    for(int i=a-1;i>=0;i--){
+        cout<<x[i]<<" ";
+    }
+
+
+
+    return 0;
+}
